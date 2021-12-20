080727f09fd3eac271f4a3c4775ca56ad542dd82
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2021-12-20 05:48:55 +0100

    Merge remote-tracking branch 'origin/printFour' into printThree
    Conflicts:
    	practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java

--- practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
+++ practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
@@ -3,17 +3,12 @@ package practica_AbdelmajidGhaiati;
 public class exercici3
 {
 	public static void main(String[] args) {
-		printOne();
-		printFour();
-	}
-	public static void printOne() {
-		System.out.println("Hello World");
-	}
-	public static void printFour() {
-		System.out.println("Hello World");
-	}
-	
 
-	
+		printThree();		
+	}
+
+	public static void printThree() {
+		System.out.println("Hello World");
+	}	
 }
 
