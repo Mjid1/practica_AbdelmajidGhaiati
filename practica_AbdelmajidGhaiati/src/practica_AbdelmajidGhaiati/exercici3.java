ad9fbdea8d7de46a5dabc71cad6d04975e452858
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2021-12-20 04:42:27 +0100

    printThree

--- practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
+++ practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
@@ -4,13 +4,16 @@ public class exercici3
 {
 	public static void main(String[] args) {
 		printOne();
-		printOne();
-		printTwo();
+		printOne();		
 	}
 
 	public static void printOne() {
 		System.out.println("Hello World");
 	}
+	public static void printThree() {
+		System.out.println("Hello World");
+	}
+
 	
 }
 
