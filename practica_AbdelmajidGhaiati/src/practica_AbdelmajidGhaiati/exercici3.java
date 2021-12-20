c36e96e0590cb087214bc32cf58e1b1f8e40b4c1
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2021-12-20 05:35:25 +0100

    PrintFour

--- practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
+++ practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
@@ -3,15 +3,7 @@ package practica_AbdelmajidGhaiati;
 public class exercici3
 {
 	public static void main(String[] args) {
-		printOne();
-		printOne();
-	}
-
-	public static void printOne() {
-		System.out.println("Hello World");
-	}
-	public static void printThree() {
-		System.out.println("Hello World");
+		printFour();
 	}
 	public static void printFour() {
 		System.out.println("Hello World");
