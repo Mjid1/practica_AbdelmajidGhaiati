ce1b4c6bf980c0a066bdaf21c96faa28591d9ec8
Abdelmajid Ghaiati <a2mjidgh@gmail.com>
2021-12-20 05:24:12 +0100

    esborra el mètode printTwo

--- practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
+++ practica_AbdelmajidGhaiati/src/practica_AbdelmajidGhaiati/exercici3.java
@@ -5,17 +5,11 @@ public class exercici3
 	public static void main(String[] args) {
 		printOne();
 		printOne();
-		printTwo();
 	}
 
 	public static void printOne() {
 		System.out.println("Hello World");
 	}
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
-
-	
+		
 }
 
